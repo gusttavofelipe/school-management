058db95712c0a89c428805805d3bad7763e02a52
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2023-10-10 11:49:56 -0300

    add - docker compose e env

--- env.py
+++ env.py
@@ -1,11 +1,11 @@
 from decouple import config
 
 
-NAME = config("NAME")
-HOST = config("HOST")
-PORT = config("PORT")
-USER = config("USER")
-PASSWORD = config("PASSWORD")
+POSTGRES_DB = config("POSTGRES_DB")
+POSTGRES_HOST = config("POSTGRES_HOST")
+POSTGRES_PORT = config("POSTGRES_PORT")
+POSTGRES_USER = config("POSTGRES_USER")
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD")
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG")
 
-
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
